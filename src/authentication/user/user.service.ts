fdbd1ffd169f82552a4d7e1af0b85643d2bc9ba6
zadlyka <tara02zadlyka@gmail.com>
2023-07-01 16:45:21 +0700

    implement caching

--- src/authentication/user/user.service.ts
+++ src/authentication/user/user.service.ts
@@ -1,17 +1,22 @@
 import * as bcrypt from 'bcrypt';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginateQuery, paginate, FilterOperator } from 'nestjs-paginate';
 import { Repository } from 'typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
+const key = 'user';
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
@@ -21,11 +26,15 @@ export class UserService {
       : await bcrypt.hash('not-set', 10);
     const user = this.userRepository.create({ ...attrsUser, password: hash });
     await this.userRepository.save(user);
+    await this.cacheManager.del(key);
     return this.findOne(user.id);
   }
 
-  findAll(query: PaginateQuery) {
-    return paginate(query, this.userRepository, {
+  async findAll(query: PaginateQuery) {
+    const cache = await this.cacheManager.get(key);
+    if (cache) return cache;
+
+    const item = await paginate(query, this.userRepository, {
       sortableColumns: ['id', 'name'],
       nullSort: 'last',
       defaultSortBy: [['id', 'DESC']],
@@ -35,6 +44,8 @@ export class UserService {
         email: [FilterOperator.EQ],
       },
     });
+    await this.cacheManager.set(key, item);
+    return item;
   }
 
   findOne(id: string) {
@@ -47,11 +58,13 @@ export class UserService {
 
   async update(id: string, updateUserDto: UpdateUserDto) {
     await this.userRepository.update(id, updateUserDto);
+    await this.cacheManager.del(key);
     return this.findOne(id);
   }
 
   async remove(id: string) {
     const user = await this.findOne(id);
+    await this.cacheManager.del(key);
     return this.userRepository.softRemove(user);
   }
 }
