b49f38db288a9141ef28b5386f794165d50ad56c
zadlyka <tara02zadlyka@gmail.com>
2023-07-01 18:56:54 +0700

    implement task-scheduling

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CustomLogger } from './services/custom-logger.service';
+import { TaskService } from './services/task.service';
 
 @Module({
-  providers: [CustomLogger],
+  providers: [CustomLogger, TaskService],
   exports: [CustomLogger],
 })
 export class CommonModule {}
