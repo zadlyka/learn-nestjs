f9fd20a6c2a316402b1cb6e8e891bd9aa37071d6
zadlyka <tara02zadlyka@gmail.com>
2023-07-08 14:33:43 +0700

    implement e2e-testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,11 @@
+import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let accessToken: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,12 +14,23 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    const response = await request(app.getHttpServer())
+      .post('/auth')
+      .send({ username: 'admin@mail.com', password: 'not-set' });
+
+    accessToken = response.body.data.accessToken;
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
+      .set('Authorization', `Bearer ${accessToken}`)
       .expect(200)
-      .expect('Hello World!');
+      .expect({ statusCode: 200, data: 'Hello World!' });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
